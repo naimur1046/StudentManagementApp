0b32370767b97fe0545b0db60680c7899bee3e2f
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-08 15:10:53 +0600

    Add project files.

---
+++ Model/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementApp.Model
+{
+    public class Student
+    {
+        public virtual int rollNumber { get; set; }
+        public virtual string firstName { get; set; }
+        public virtual string lastName { get; set; }
+        public virtual string department { get; set; }
+        public virtual string mobileNumber { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StudentManagementApp.View;
+using System.Reflection;
+using System;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+namespace StudentManagementApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Screen normalScreen = new Screen();
+
+            normalScreen.MainScreen();
+
+        }
+
+    }
+}
