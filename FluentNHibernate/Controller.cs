90e9540628ef645b7c57816a570182d0201bcffb
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-11 21:41:42 +0600

    Learned and integrated FluentNHibernate for ORM mapping in Student Management System

---
+++ FluentNHibernate/Controller.cs
@@ -0,0 +1,37 @@
+﻿using StudentManagementApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Conventions.Inspections;
+using StudentManagementApp.Model;
+using StudentManagementApp.FluentNHibernate;
+
+
+
+namespace StudentManagementApp.FluentNHibernate
+{
+    public class Controller
+    {
+        public Controller() {
+            using (var session = NHibernateHelper.GetSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var product = new Product()
+                    {
+                        Id = 10,
+                        Name = "Hi",
+                        Units = 2,
+                        Price = 3,
+                    };
+                    session.Save(product);
+                    transaction.Commit(); // Commit transaction
+                }
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
 using StudentManagementApp.UI;
+using StudentManagementApp.FluentNHibernate;
 
 
 namespace StudentManagementApp
@@ -14,14 +15,14 @@ namespace StudentManagementApp
     {
         public static void Main(string[] args)
         {
-            
 
-            
 
+
+            Controller FluentNHibernate = new Controller();
 
 
             //normalScreen.MainScreen();
-
+            
             StudentUI studentUI = new StudentUI();
             
 
